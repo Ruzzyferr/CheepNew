ab975ebed2af7aa58dfd0cdab65b392e258c0845
Ruzzyferr <bulutruzgaremir@gmail.com>
2024-03-06 18:55:27 +0300

    asd

---
+++ src/main/java/com/example/cheep/CheepApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cheep/CheepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
